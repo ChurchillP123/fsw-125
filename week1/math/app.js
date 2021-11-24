f3dfae4009be6fa23007c6e8611ec95655294530
Churchill perry <churchill.perry@bryanuniversity.edu>
2021-11-23 23:54:43 -0600

    node basics export and require modules

---
+++ week1/math/app.js
@@ -0,0 +1,10 @@
+const mathObj = require('./math.js');
+
+//add
+console.log(mathObj.add(1, 2, 3)); 
+//subtract
+console.log(mathObj.subtract(1, 2, 3));
+//multiple
+console.log(mathObj.multiply(1, 2, 3));
+//divide
+console.log(mathObj.divide(1, 2, 3));
